7d5643e1d67473247b9fe1e798a5751fb1e566b2
Marc-Henri00 <marcallou45@gmail.com>
2026-01-06 11:56:11 +0100

    mise a jour du projet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./*.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/**/*.{html,js}"
+  ],
   theme: {
     extend: {},
   },
